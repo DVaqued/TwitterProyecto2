7cee41722e7615976e2ae1cab2ea6032cfad9bf8
Cjuarez45 <Cjuarez45@CJuarez>
2024-09-04 19:58:55 -0600

    haciendo que solo permita 140 caracters el tweet

--- src/login/InicioSesion.java
+++ src/login/InicioSesion.java
@@ -32,12 +32,13 @@ public class InicioSesion extends javax.swing.JFrame {
         cerrarSesion = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
+        jTextPane1 = new javax.swing.JTextPane();
         usuarioActual = new javax.swing.JLabel();
         perfil = new javax.swing.JButton();
         publicarTwit = new javax.swing.JButton();
         hashtags = new javax.swing.JButton();
         jLabel3 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
+        jtweetfield = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -56,6 +57,9 @@ public class InicioSesion extends javax.swing.JFrame {
 
         jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/logo_twitter/twitter-removebg-preview (1).png"))); // NOI18N
         jLabel2.setText("jLabel2");
+
+        jTextPane1.setEditable(false);
+        jScrollPane1.setViewportView(jTextPane1);
 
         usuarioActual.setText(usuario.usuarioSesion);
 
@@ -77,14 +81,18 @@ public class InicioSesion extends javax.swing.JFrame {
 
         jLabel3.setText("Twitter Timeline");
 
-        jTextField1.setText("jTextField1");
-        jTextField1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField1ActionPerformed(evt);
+        jtweetfield.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                jtweetfieldKeyTyped(evt);
             }
         });
 
-        jButton1.setText("jButton1");
+        jButton1.setText("Publicar");
+        jButton1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton1MouseClicked(evt);
+            }
+        });
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
@@ -126,7 +134,7 @@ public class InicioSesion extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 857, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 706, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jtweetfield, javax.swing.GroupLayout.PREFERRED_SIZE, 706, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                 .addGap(0, 0, Short.MAX_VALUE))
@@ -151,7 +159,7 @@ public class InicioSesion extends javax.swing.JFrame {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, 43, Short.MAX_VALUE)
-                            .addComponent(jTextField1)))
+                            .addComponent(jtweetfield)))
                     .addComponent(usuarioActual))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -188,9 +196,13 @@ public class InicioSesion extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_perfilActionPerformed
 
-    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField1ActionPerformed
+    private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseClicked
+        jtweetfield.setText("");
+    }//GEN-LAST:event_jButton1MouseClicked
+
+    private void jtweetfieldKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jtweetfieldKeyTyped
+     if(jtweetfield.getText().length()>=140){}
+    }//GEN-LAST:event_jtweetfieldKeyTyped
     public void setUsuarioActual(String usuarioA) {
         this.usuarioA = usuarioA;
         usuarioActual.setText(usuarioA);
@@ -240,7 +252,8 @@ public class InicioSesion extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextPane jTextPane1;
+    private javax.swing.JTextField jtweetfield;
     private javax.swing.JButton perfil;
     private javax.swing.JButton publicarTwit;
     private javax.swing.JLabel usuarioActual;
